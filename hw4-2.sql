f0cdd57c0179590c8bdbd0d628ecb37c4596ee19
haylee360 <hayleeoyler360@gmail.com>
2025-04-29 16:56:20 -0700

    HW 4

--- hw4-2.sql
+++ hw4-2.sql
@@ -1,7 +1,9 @@
 -- Week 4 - Who worked with whom?
 -- Haylee Oyler
 
-SELECT Site A.Observer AS Observer_1 B.Observer AS Observer_2 FROM Camp_assignment A JOIN Camp_assignment B 
+
+SELECT A.Site, A.Observer AS Observer_1, B.Observer AS Observer_2  
+    FROM Camp_assignment A JOIN Camp_assignment B 
     ON A.Site = B.Site
     AND ((A.Start <= B.End)  AND (A.End >= B.Start))
     WHERE (A.Site = 'lkri' AND A.Observer < B.Observer);
